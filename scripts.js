6505cb6fa6b4cbdf9d0016e239e8f1ace324990e
Jessica Smithers <jessicaesmithers@gmail.com>
2023-02-27 15:32:35 -0500

    Add limitation on number of decimals, digits

--- scripts.js
+++ scripts.js
@@ -16,13 +16,32 @@ let answer = "";
 //anytime a number button is pressed...
 for(let i of numBtns){
    i.addEventListener("click", function(){
-        if(!operator){
-            firstNum += this.innerText;
-            display.innerText = firstNum;
+        //check if it already contains a decimal
+        if(this.innerText == "."){
+            if(!operator){
+                if(!firstNum.includes(".")){
+                    firstNum += this.innerText;
+                    display.innerText = firstNum;
+                }
+            } else {
+                if(!secondNum.includes(".")){
+                    secondNum += this.innerText;
+                    display.innerText = secondNum;
+                }
+            }
         } else {
-            secondNum += this.innerText;
-            display.innerText = secondNum;
-        }
+            if(!operator){
+                if(firstNum.length < 13){
+                    firstNum += this.innerText;
+                    display.innerText = firstNum;
+                }
+            } else {
+                if(secondNum.length < 13){
+                    secondNum += this.innerText;
+                    display.innerText = secondNum;
+                }
+            }
+    }
    });
 }
 
@@ -42,17 +61,44 @@ for(let i of operators){
 
 equals.addEventListener("click", operate);
 clearBtn.addEventListener("click", clearAll);
+deleteBtn.addEventListener("click", backspace);
 
 function operate(){
     if(firstNum && secondNum && operator){
         if(operator == "+") answer = (add(+firstNum, +secondNum));
         if(operator == "-") answer = (subtract(+firstNum, +secondNum));
         if(operator == "x") answer = (multiply(+firstNum, +secondNum));
-        if(operator == "/") answer = (divide(+firstNum, +secondNum));
-        display.innerText = answer;
-        firstNum = answer;
+        //checks for division by 0 errors
+        if(operator == "/" && secondNum != 0){
+            answer = (divide(+firstNum, +secondNum));
+        } else if (operator == "/" && secondNum == 0){
+            alert("Error! Cannot divide by zero.");
+        }
+        //ensures that the answer doesn't overflow from display
+        if(answer.toString().length > 13){
+            if(answer > 9999999999999){
+                display.innerText = answer.toPrecision(9);
+            } else {
+                display.innerText = (answer).toString().substring(0, 9);
+            }
+        } else {
+            display.innerText = answer;
+        }
+        firstNum = answer.toString();
         secondNum = "";
         operator = "";
+    }
+}
+
+function backspace(){
+    if(!operator){
+        firstNum = firstNum.slice(0, firstNum.length - 1);
+        display.innerText = firstNum;
+    } else {
+        if(secondNum){
+            secondNum = secondNum.slice(0, secondNum.length - 1);
+            display.innerText = secondNum;
+        }
     }
 }
 
